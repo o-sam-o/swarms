1f388924303e0dcfa795dabf473eaeefc200bf49
Sam Cavenagh <cavenaghweb@hotmail.com>
2010-07-25 15:14:02 +1000

    add spec to torrent_soruce

--- spec/lib/source/torrent_source_spec.rb
+++ spec/lib/source/torrent_source_spec.rb
@@ -2,6 +2,23 @@ require 'spec_helper'
 
 describe Source::TorrentSource do
   
-  it 'should call the pirate bay soruce'
+  it 'should call the pirate bay soruce' do
+    pb_source = mock(:pirate_bay_source)
+    Source::PirateBaySource.should_receive(:new).and_return(pb_source)
+    pb_source.should_receive(:refresh)
+    
+    Source::TorrentSource.update
+  end  
+  
+  it 'should update movie swarm_scores' do
+    pb_source = mock(:pirate_bay_source).as_null_object
+    Source::PirateBaySource.stub!(:new).and_return(pb_source)
+    
+    movie = mock(:movie)
+    Movie.should_receive(:find_each).and_yield(movie)
+    movie.should_receive(:update_swarm_score)
+    
+    Source::TorrentSource.update
+  end  
   
 end  
