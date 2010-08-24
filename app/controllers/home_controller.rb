aeaaa64164df8a2f7840f8003d768159eaefb54f
Sam Cavenagh <cavenaghweb@hotmail.com>
2010-08-24 21:11:37 +0100

    Change homepage to not display movies without a rank

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   
   def index
-    @movies = Movie.includes(:torrents => :torrent_stats).order('swarm_score desc').paginate(:page => params[:page], :per_page => 15)
+    @movies = Movie.includes(:torrents => :torrent_stats)
+                   .where('current_rank != 0')
+                   .order('current_rank')
+                   .paginate(:page => params[:page], :per_page => 15)
   end
 
 end
