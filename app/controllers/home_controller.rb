78532f35a4a531726bcb6f0d061d63983d85978c
Sam Cavenagh <cavenaghweb@hotmail.com>
2010-09-12 19:42:32 +1000

    Improve hompage size bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
                    .order('current_rank')
                    .paginate(:page => params[:page], :per_page => 15)
 
-    @featured_movie = Movie.where('current_rank != 0').order('current_rank').first
+    @recent_additions = Movie.where('current_rank != 0').order('created_at DESC').limit(5)
   end
 
 end
