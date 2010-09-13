ce456fe86510372ae9e790afd385d4e99d44224e
Sam Cavenagh <cavenaghweb@hotmail.com>
2010-09-13 21:55:01 +1000

    remove unneeded home page join

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @movies = Movie.includes(:torrents => :torrent_stats)
-                   .where('current_rank != 0')
+    @movies = Movie.where('current_rank != 0')
                    .order('current_rank')
                    .paginate(:page => params[:page], :per_page => 15)
 
