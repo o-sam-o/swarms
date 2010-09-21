ecf221a3fa188a0ce1fee1231dfe6161ac2a08ec
Sam Cavenagh <cavenaghweb@hotmail.com>
2010-09-21 21:44:22 +1000

    Add genres to page footer + support filtering by genre

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  before_filter :load_layout_vars
+
+private
+
+  def load_layout_vars
+    @genres = Genre.order(:name)
+  end  
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,12 @@ class HomeController < ApplicationController
     @movies = Movie.where('current_rank != 0')
                    .order('current_rank')
                    .includes(:movie_images)
-                   .paginate(:page => params[:page], :per_page => 15)
+
+    unless params[:genre].blank?
+       @movies = @movies.joins(:genres).where('genres.name' => params[:genre])
+    end
+
+    @movies = @movies.paginate(:page => params[:page], :per_page => 15)
 
     @recent_additions = Movie.where('current_rank != 0').order('created_at DESC').limit(5)
   end
